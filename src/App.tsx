b1ad8eb0bf9f902380d018ebfd7b7f8f85947320
Enoque Ferreira <ynickbr6@gmail.com>
2026-09-04 19:44:10 -0300

    feat: setup inicial do projeto resolveai_cliente e esqueleto do style guide

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 style={ {color: 'var(--color-primary)', fontSize: 'var(--font-size-lg)' }}>Olá, Mundo! 🌎</h1>
+    </>
+  )
+}
+
+export default App
